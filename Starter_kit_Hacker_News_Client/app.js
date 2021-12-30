b1c3d004cd96b3c0f19581c0b583df9c720d9175
funkyblues <dnr8874@naver.com>
2021-12-30 20:20:17 +0900

    JavaScript Starter Kit_Hacker News Client Ch.3-3

--- Starter_kit_Hacker_News_Client/app.js
+++ Starter_kit_Hacker_News_Client/app.js
@@ -1,9 +1,40 @@
 const ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 
 // false : 비동기가 아닌, 동기적으로 처리하겠다는 의미의 코드
-ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
+ajax.open("GET", NEWS_URL, false);
 
 // ajax.send()를 호출하면 데이터를 가져옴
 ajax.send();
 
 // 데이터는 ajax의 response라는 곳에 들어있다.
+// console.log(ajax.response);
+
+// 네트워크를 통해 가져온 데이터를 어떻게 다뤄야 하나?
+// 개발자도구
+// XHR : XHLHttpRequest
+// 네트워크를 가져온 항목들만 보여주는 필터라고 보면 됨
+// 1.json 을 클릭하면 response에서 console.log한 내용과 동일한 내용을 보여줌!
+
+
+//json 데이터를 객체로
+const newsFeed = JSON.parse(ajax.response);
+// console.log(newsFeed);
+
+// ul태그 반환.
+const ul = document.createElement("ul");
+
+for(let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
+
+
+// document.getElementById('root').innerHTML = `<ul>
+//   <li>${newsFeed[0].title}</li>
+//   <li>${newsFeed[1].title}</li>
+//   <li>${newsFeed[2].title}</li>
+// </ul>`;
--- Starter_kit_Hacker_News_Client/dist/index.833266ea.js
+++ Starter_kit_Hacker_News_Client/dist/index.833266ea.js
@@ -140,7 +140,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"b6SDB":[function(require,module,exports) {
+})({"a02YD":[function(require,module,exports) {
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
@@ -460,11 +460,34 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"lzYRN":[function(require,module,exports) {
 const ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 // false : 비동기가 아닌, 동기적으로 처리하겠다는 의미의 코드
-ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
+ajax.open("GET", NEWS_URL, false);
 // ajax.send()를 호출하면 데이터를 가져옴
-ajax.send(); // 데이터는 ajax의 response라는 곳에 들어있다.
+ajax.send();
+// 데이터는 ajax의 response라는 곳에 들어있다.
+// console.log(ajax.response);
+// 네트워크를 통해 가져온 데이터를 어떻게 다뤄야 하나?
+// 개발자도구
+// XHR : XHLHttpRequest
+// 네트워크를 가져온 항목들만 보여주는 필터라고 보면 됨
+// 1.json 을 클릭하면 response에서 console.log한 내용과 동일한 내용을 보여줌!
+//json 데이터를 객체로
+const newsFeed = JSON.parse(ajax.response);
+// console.log(newsFeed);
+// ul태그 반환.
+const ul = document.createElement("ul");
+for(let i = 0; i < 10; i++){
+    const li = document.createElement("li");
+    li.innerHTML = newsFeed[i].title;
+    ul.appendChild(li);
+}
+document.getElementById("root").appendChild(ul); // document.getElementById('root').innerHTML = `<ul>
+ //   <li>${newsFeed[0].title}</li>
+ //   <li>${newsFeed[1].title}</li>
+ //   <li>${newsFeed[2].title}</li>
+ // </ul>`;
 
-},{}]},["b6SDB","lzYRN"], "lzYRN", "parcelRequire94c2")
+},{}]},["a02YD","lzYRN"], "lzYRN", "parcelRequire94c2")
 
 //# sourceMappingURL=index.833266ea.js.map
